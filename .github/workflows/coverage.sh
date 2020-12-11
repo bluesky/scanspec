1710b44a7c29c5a1a8a210a66a9beb75ca008a96
Tom Cobb <tom.cobb@diamond.ac.uk>
2020-12-11 15:24:57 +0000

    Fix coverage ranges

--- .github/workflows/coverage.sh
+++ .github/workflows/coverage.sh
@@ -8,9 +8,9 @@
 total=$(grep 'TOTAL    ' ${1} | grep -Po '[0-9]+(?=%$)')
 echo "Generating badge for Total Coverage of $total"
 
-if (( $(echo "$total <= 60" | bc -l) )) ; then
+if (( $(echo "$total < 70" | bc -l) )) ; then
     COLOR=red
-elif (( $(echo "$total > 99" | bc -l) )); then
+elif (( $(echo "$total >= 90" | bc -l) )); then
     COLOR=green
 else
     COLOR=orange
