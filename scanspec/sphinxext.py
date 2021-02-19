e8aed40c3338bc119c62a9066ed6d7b999d725ca
Emma Dixon <emma.dixon@diamond.ac.uk>
2021-02-19 10:05:02 +0000

    Fix sphinxext for apischema
    This needs further refactoring

--- scanspec/sphinxext.py
+++ scanspec/sphinxext.py
@@ -1,99 +1,109 @@
-from inspect import Parameter, signature
-from typing import Iterator, List, Optional, Tuple
+from dataclasses import _FIELD_CLASSVAR, Field, is_dataclass  # type: ignore
+from typing import Annotated, Any, Dict, Optional, Tuple, get_args, get_origin
 
+from apischema.json_schema.schema import Schema
+from apischema.metadata.keys import SCHEMA_METADATA
 from matplotlib.sphinxext.plot_directive import PlotDirective
-from pydantic import BaseModel
-from pydantic.decorator import ValidatedFunction
-from pydantic.typing import display_as_type
-
-from scanspec.specs import _modifier
 
 
-def validated_function_ignores(vd: ValidatedFunction) -> Iterator[str]:
-    # Pydantic adds args and kwargs, remove them if not in original func
-    parameters = signature(vd.raw_function).parameters
-    kinds = {p.kind for p in parameters.values()}
-    if Parameter.VAR_POSITIONAL not in kinds:
-        yield vd.v_args_name
-    if Parameter.VAR_KEYWORD not in kinds:
-        yield vd.v_kwargs_name
-    # Ignore the first arg as it is the classname
-    # This wouldn't work for @staticmethod, but we don't have any
-    yield list(parameters)[0]
+def get_type(meta: Tuple[Any]) -> str:
+    try:
+        field_type = meta[0].__name__
+    except AttributeError:
+        field_type = meta[0]._name
+    return field_type
 
 
-def get_model_ignores(what: str, obj) -> Tuple[Optional[BaseModel], List[str]]:
-    model = None
-    ignores = []
-    if what == "class" and issubclass(obj, BaseModel):
-        # If we are a BaseModel, use this
-        model = obj
-    elif what == "method" and issubclass(getattr(obj, "model", type), BaseModel):
-        # If we have been decorated with validate_arguments, use the model
-        model = obj.model
-        ignores = list(validated_function_ignores(obj.vd))
-    if model:
-        for name, field in model.__fields__.items():
-            if field.field_info.const and not field.required:
-                # Suppress this one as it is probably used like this:
-                # https://pydantic-docs.helpmanual.io/usage/types/#literal-type
-                ignores.append(name)
-    return model, ignores
+def get_dataclass_fields(obj: Any) -> Dict[str, Field]:
+    fields = obj.__dataclass_fields__
+    # Remove class variables
+    fields = {
+        field_name: field
+        for field_name, field in fields.items()
+        if field._field_type != _FIELD_CLASSVAR
+    }
+    return fields
+
+
+def get_metadata(what: str, obj) -> Optional[Dict[str, Any]]:
+    metadata = None
+    if what == "class" and is_dataclass(obj):
+        # If we are a dataclass, use this
+        fields = get_dataclass_fields(obj)
+        metadata = get_metadata_from_fields(fields)
+    elif what == "method":
+        annotations = obj.__annotations__
+        # Don't include the return value
+        annotations = {
+            name: annotation
+            for name, annotation in annotations.items()
+            if name != "return"
+        }
+        metadata = get_metadata_from_annotations(annotations)
+    return metadata
+
+
+def get_metadata_from_fields(fields: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+    metadata = {}
+    for name, field in fields.items():
+        metadata[name] = (field.type, field.metadata)
+    return metadata or None
+
+
+def get_metadata_from_annotations(
+    annotations: Dict[str, Any]
+) -> Optional[Dict[str, Any]]:
+    metadata = {}
+    for name, annotation in annotations.items():
+        if get_origin(annotation) == Annotated:
+            metadata[name] = get_args(annotation)
+    return metadata or None
+
+
+def get_description(meta: Any) -> str:
+    description = ""
+    additional = []
+    for arg in meta[1:]:
+        arg = arg.get(SCHEMA_METADATA, arg)
+        if type(arg) == Schema:
+            field_schema = arg.as_dict()
+            for key, value in field_schema.items():
+                if key == "description":
+                    description = value
+                else:
+                    additional.append(f"{str(key)}: {str(value)}")
+    if additional:
+        description = description + " - " + ", ".join(additional)
+    return description
 
 
 def process_docstring(app, what, name, obj, options, lines):
-    model, ignores = get_model_ignores(what, obj)
-    if model:
-        # Insert it after the first gap
+    # Must also work for the alternative constructors such as bounded!!
+    metadata = get_metadata(what, obj)
+    if metadata:
         try:
             index = lines.index("") + 1
         except ValueError:
             lines.append("")
             index = len(lines)
         # Add types from each field
-        for name, field in model.__fields__.items():
-            if name not in ignores:
-                # UnionModifier expanded this so creation from
-                # JSON works, put the original back for docs
-                if field.type_ is _modifier.spec_union:
-                    type_string = "Spec"
-                elif field.type_ is _modifier.region_union:
-                    type_string = "Region"
-                else:
-                    type_string = display_as_type(field.type_)
-                description = field.field_info.description
-                if type_string == "ConstrainedIntValue":
-                    type_string = "int"
-                    translate = dict(
-                        gt=">", ge=">=", lt="<", le="<=", multiple_of="multiple of"
-                    )
-                    conditions = []
-                    for attr, text in translate.items():
-                        v = getattr(field.type_, attr)
-                        if v:
-                            conditions.append(f"{text} {v}")
-                    if conditions:
-                        description += f". Must be {' ,'.join(conditions)}"
-                lines.insert(
-                    index, f":param {type_string} {name}: {description}",
-                )
-                index += 1
+        for name, meta in metadata.items():
+            description = get_description(meta)
+            field_type = get_type(meta)
+            lines.insert(
+                index, f":param {field_type} {name}: {description}",
+            )
+            index += 1
         lines.insert(index, "")
 
 
 def process_signature(app, what, name, obj, options, signature, return_annotation):
-    model, ignores = get_model_ignores(what, obj)
-    if model:
-        # Recreate signature from the model
-        args = []
-        for name, field in model.__fields__.items():
-            if name not in ignores:
-                if field.required:
-                    args.append(name)
-                else:
-                    args.append(f"{name}={field.default!r}")
+    # Recreate signature from the model
+    if is_dataclass(obj):
+        fields = get_dataclass_fields(obj)
+        args = fields.keys()
         signature = f'({", ".join(args)})'
-    return signature, return_annotation
+        return signature, return_annotation
 
 
 class ExampleSpecDirective(PlotDirective):
