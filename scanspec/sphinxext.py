79464d613828cf913c19768fad80233b990a00f7
Tom Cobb <tom.cobb@diamond.ac.uk>
2021-08-17 15:31:12 +0100

    Force sphinxext to always make figures

--- scanspec/sphinxext.py
+++ scanspec/sphinxext.py
@@ -1,16 +1,33 @@
-from matplotlib.sphinxext.plot_directive import PlotDirective
+from contextlib import contextmanager
+
+from matplotlib.sphinxext import plot_directive
 
 from ._version_git import __version__
 
 
-class ExampleSpecDirective(PlotDirective):
+@contextmanager
+def always_create_figures():
+    """Force matplotlib PlotDirective to always create figures, even
+    if source rst hasn't changed, as we often use example_spec from
+    within docstrings"""
+    orig_f = plot_directive.out_of_date
+    # Patch the plot directive so it thinks all sources are out of date
+    plot_directive.out_of_date = lambda o, d: True
+    try:
+        yield
+    finally:
+        plot_directive.out_of_date = orig_f
+
+
+class ExampleSpecDirective(plot_directive.PlotDirective):
     def run(self):
         self.content = (
             ["# Example Spec", "", "from scanspec.plot import plot_spec"]
             + [str(x) for x in self.content]
             + ["plot_spec(spec)"]
         )
-        return super().run()
+        with always_create_figures():
+            return super().run()
 
 
 def setup(app):
