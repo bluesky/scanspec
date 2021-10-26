8791d75198d0a1bc971316d5db60f9e180b162dc
thomascobb <thomascobb@users.noreply.github.com>
2021-10-26 11:15:44 +0000

    deploy: 583ae6936bca22be7de4092cbc921407b38b4887

--- delete/_static/documentation_options.js
+++ delete/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.2+1.g7263da7',
+    VERSION: '0.5.2+2.g583ae69',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
