d19b76b92f517c0d2c5badd74257fda5aab39b04
thomascobb <thomascobb@users.noreply.github.com>
2021-10-26 12:42:31 +0000

    deploy: 2a3ff7a0e42eea643ce2890f4bd056b76c0e659e

--- merge/_static/documentation_options.js
+++ merge/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.2+3.ge85a597',
+    VERSION: '0.5.2+3.g2a3ff7a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
