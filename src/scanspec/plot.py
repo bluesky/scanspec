22ae64c64e3bd00a7fbbd3cf89a10e880c293e73
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-01-07 10:28:06 +0000

    Remove duplicate draw logic

--- src/scanspec/plot.py
+++ src/scanspec/plot.py
@@ -29,12 +29,6 @@ class _Arrow3D(patches.FancyArrowPatch):
         super().__init__((0, 0), (0, 0), *args, **kwargs)
         self._verts3d = xs, ys, zs
 
-    def draw(self, renderer):
-        xs3d, ys3d, zs3d = self._verts3d
-        xs, ys, _ = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
-        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
-        super().draw(renderer)
-
     # Added here because of https://github.com/matplotlib/matplotlib/issues/21688
     def do_3d_projection(self, renderer=None):
         xs3d, ys3d, zs3d = self._verts3d
