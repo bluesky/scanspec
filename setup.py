198f4092b1e3830fad8af86dcae45de1a8069f38
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-04-28 17:07:04 +0100

    Simplify

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 # type: ignore
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        "write_to": "src/scanspec/_version.py"
-    },
-    setup_requires=["setuptools>=45,<57", "setuptools_scm[toml]>=6.2", "wheel==0.33.1"],
-)
+setup(use_scm_version={"write_to": "src/scanspec/_version.py"})
