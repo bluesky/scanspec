df7a7ca110574267781e564dbb496736321f264c
tizayi <tizayi@users.noreply.github.com>
2023-01-05 18:06:38 +0000

    deploy: 73a8c38b1308b8eb375efecbb4f5b01ec9ba7261

--- matplotlib-update/_static/documentation_options.js
+++ matplotlib-update/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.6.dev5+gb4af932',
+    VERSION: '0.5.6.dev6+g73a8c38',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
