0d416c2b0010b53e3d7124b0d25ab724aa07146b
callumforrester <callumforrester@users.noreply.github.com>
2023-01-19 09:09:48 +0000

    deploy: 73e9a11ea86b759d66a207cfa578ec3a9d869ed3

--- fastapi-and-pydantic/_static/documentation_options.js
+++ fastapi-and-pydantic/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.6.dev36+g74a4de3',
+    VERSION: '0.5.6.dev37+g73e9a11',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
