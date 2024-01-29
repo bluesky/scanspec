cb848f3048de70bb459d798a103c11feb2682a3c
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-01-29 10:17:01 +0000

    Move function for getting frame duration from dls-bluesky-core (from i22-bluesky) to ScanSpec

--- tests/test_specs.py
+++ tests/test_specs.py
@@ -1,3 +1,4 @@
+import re
 from typing import Any, Tuple
 
 import pytest
@@ -16,6 +17,7 @@ from scanspec.specs import (
     Static,
     Zip,
     fly,
+    get_constant_duration,
     step,
 )
 
@@ -558,3 +560,61 @@ def test_multiple_statics_with_grid():
 )
 def test_shape(spec: Spec, expected_shape: Tuple[int, ...]):
     assert expected_shape == spec.shape()
+
+
+def test_single_frame_single_point():
+    spec = Static.duration(0.1)
+    assert get_constant_duration(spec.calculate()) == 0.1
+
+
+def test_consistent_points():
+    spec = Static.duration(0.1).concat(Static.duration(0.1))
+    assert get_constant_duration(spec.calculate()) == 0.1
+
+
+def test_inconsistent_points():
+    spec = Static.duration(0.1).concat(Static.duration(0.2))
+    assert get_constant_duration(spec.calculate()) is None
+
+
+def test_frame_with_multiple_axes():
+    spec = Static.duration(0.1).zip(Line.bounded("x", 0, 0, 1))
+    frames = spec.calculate()
+    assert len(frames) == 1
+    assert get_constant_duration(frames) == 0.1
+
+
+def test_inconsistent_frame_with_multiple_axes():
+    spec = (
+        Static.duration(0.1)
+        .concat(Static.duration(0.2))
+        .zip(Line.bounded("x", 0, 0, 2))
+    )
+    frames = spec.calculate()
+    assert len(frames) == 1
+    assert get_constant_duration(frames) is None
+
+
+def test_non_static_spec_duration():
+    spec = Line.bounded(DURATION, 0, 0, 3)
+    frames = spec.calculate()
+    assert len(frames) == 1
+    assert get_constant_duration(frames) == 0
+
+
+def test_multiple_duration_frames():
+    spec = (
+        Static.duration(0.1)
+        .concat(Static.duration(0.2))
+        .zip(Line.bounded(DURATION, 0, 0, 2))
+    )
+    with pytest.raises(
+        AssertionError, match=re.escape("Zipping would overwrite axes ['DURATION']")
+    ):
+        spec.calculate()
+    spec = (  # TODO: refactor when https://github.com/dls-controls/scanspec/issues/90
+        Static.duration(0.1) * Line.bounded(DURATION, 0, 0, 2)
+    )
+    frames = spec.calculate()
+    assert len(frames) == 2
+    assert get_constant_duration(frames) is None
