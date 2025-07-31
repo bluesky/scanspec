8651719ca0a046f40b4626b67dde9c41f4089d7b
LuisFSegalla <LuisFSegalla@users.noreply.github.com>
2025-07-31 14:18:07 +0000

    deploy: 117d29542aa99c68a00ca722c95a7301cae73e84

--- main/_downloads/5fecf49fd9e18b0cbc137cab2570ce30/scanspec-specs-12.py
+++ main/_downloads/5fecf49fd9e18b0cbc137cab2570ce30/scanspec-specs-12.py
@@ -1,7 +1,7 @@
 # Example Spec
 
 from scanspec.plot import plot_spec
-from scanspec.specs import Line, Static
+from scanspec.specs import Spiral
 
-spec = Line("y", 1, 2, 3).zip(Static.duration(0.1))
+spec = Spiral("x", "y", 1, 5, 10, 50, 30)
 plot_spec(spec)
--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.8.1.dev2+g0fd755c7',
+    VERSION: '0.8.1.dev3+g117d2954',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
